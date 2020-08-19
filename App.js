b46d913cb25c3c49fffcfe7866ac62b1bff05bf1
Doğukan <Myfa_96@hotmail.com>
2020-08-20 00:34:27 +0300

    vector-icons

--- App.js
+++ App.js
@@ -8,11 +8,15 @@
 
 import React from 'react';
 import {StyleSheet, ScrollView, View, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const App = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hello World</Text>
+      <View style={{alignItems: 'center'}}>
+        <Icon name="ios-book" color="#4F8EF7" size={100} />
+      </View>
     </View>
   );
 };
