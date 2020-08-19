26cdee343b59f4a12f5f7eeb33cb70db4216df4b
Doğukan <Myfa_96@hotmail.com>
2020-08-20 00:46:42 +0300

    react native elements

--- App.js
+++ App.js
@@ -8,15 +8,27 @@
 
 import React from 'react';
 import {StyleSheet, ScrollView, View, Text} from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import {Card, ListItem, Button, Icon} from 'react-native-elements';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
-      <View style={{alignItems: 'center'}}>
-        <Icon name="ios-book" color="#4F8EF7" size={100} />
-      </View>
+      <Card title="HELLO WORLD" image={require('./assets/images/baby.jpg')}>
+        <Text style={{marginBottom: 10}}>
+          The idea with React Native Elements is more about component structure
+          than actual design.
+        </Text>
+        <Button
+          icon={<Icon name="arrow-forward" color="#ffffff" />}
+          buttonStyle={{
+            borderRadius: 0,
+            marginLeft: 0,
+            marginRight: 0,
+            marginBottom: 0,
+          }}
+          title="VIEW NOW"
+        />
+      </Card>
     </View>
   );
 };
@@ -27,12 +39,6 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'stretch',
-  },
-
-  title: {
-    fontSize: 50,
-    color: '#f00000',
-    textAlign: 'center',
   },
 });
 
